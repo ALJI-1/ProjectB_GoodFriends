35ee5e45eba53100ffc0f9083802a0bd4ddc0f38
ALJI-1 <albin.ingelsson1@gmail.com>
2025-11-28 11:31:39 +0100

    .

--- AppMvc/Controllers/HomeController.cs
+++ AppMvc/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AppMvc.Models;
+using Services.Interfaces;
 
 namespace AppMvc.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IAddressesService _addressService;
+    private readonly IAdminService _adminService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IAddressesService addressesService, IAdminService adminService)
     {
         _logger = logger;
+        _addressService = addressesService;
+        _adminService = adminService;
     }
 
     public IActionResult Index()
@@ -21,6 +26,20 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    [HttpGet]
+    public async Task <IActionResult> Overview()
+    {
+        var addresses = await _addressService.ReadAddressesAsync(true, false, "Denmark", 0, 10);
+        var info = await _adminService.GuestInfoAsync();
+
+        var model = new OverviewViewModel
+        {
+            CountryInfo = info.Item.Friends.Where(i => i.Country == "Denmark")
+        };
+
+        return View(model);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ AppMvc/Models/OverviewModel.cs
@@ -0,0 +1,9 @@
+namespace AppMvc.Models;
+using global::Models.DTO;
+using Models;
+
+public class OverviewViewModel
+{
+    public IEnumerable<GstUsrInfoFriendsDto> CountryInfo;
+    
+}
